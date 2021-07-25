04cfb0d181b54c2a9433305c04e43e0b3f8d04d9
Adi Skop <adiskop@gmail.com>
2021-07-25 20:26:17 +0200

    fetch function

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/travelgems";
+
+document.addEventListener('DOMContentLoaded', () => {
+    getTravelGems()
+})
+
+
+function getTravelGems() {
+    fetch(endPoint)
+      .then(res => res.json())
+      .then(travelgems => {
+        // remember our JSON data is a bit nested due to our serializer
+        travelgems.data.forEach(travelgem => {
+          // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+          const travelgemMarkup = 
+            `<div data-id=${travelgem.id}>
+              <img src=${travelgem.attributes.image_url} height="200" width="250">
+              <h3>${travelgem.attributes.title}</h3>
+              <p>${travelgem.attributes.destination.name}</p>
+              <button data-id=${travelgem.id}>edit</button>
+            </div>
+            <br><br>`;
+  
+            document.querySelector('#travelgem-container').innerHTML += travelgemMarkup
+        })
+      })
+  }
