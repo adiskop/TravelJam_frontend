465b0e69df0f6f132dc5e01cf0009557b687f3e0
Adi Skop <adiskop@gmail.com>
2021-07-26 16:39:34 +0200

    add render function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const endPoint = "http://localhost:3000/api/v1/travelgems";
 
 document.addEventListener('DOMContentLoaded', () => {
+  //fetch and load travel gems
     getTravelGems()
+
+    const createTravelgemsForm = document.querySelector("#create-travelgem-form")
+
+    createTravelgemForm.addEventListener('submit', (e) =>
+    createFormHandler(e))
 })
 
 
@@ -12,16 +18,26 @@ function getTravelGems() {
         // remember our JSON data is a bit nested due to our serializer
         travelgems.data.forEach(travelgem => {
           // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-          const travelgemMarkup = 
-            `<div data-id=${travelgem.id}>
-              <img src=${travelgem.attributes.image_url} height="200" width="250">
-              <h3>${travelgem.attributes.title}</h3>
-              <p>${travelgem.attributes.destination.name}</p>
-              <button data-id=${travelgem.id}>edit</button>
-            </div>
-            <br><br>`;
-  
-            document.querySelector('#travelgem-container').innerHTML += travelgemMarkup
+          render(travelgem)
         })
       })
-  }
+    }
+
+
+    function render(travelgem) {
+      const travelgemMarkup = 
+      `<div data-id=${travelgem.id}>
+        <img src=${travelgem.attributes.image_url} height="200" width="250">
+        <h3>${travelgem.attributes.title}</h3>
+        <p>${travelgem.attributes.destination.name}</p>
+        <button data-id=${travelgem.id}>edit</button>
+      </div>
+      <br><br>`;
+
+      document.querySelector('#travelgem-container').innerHTML += travelgemMarkup
+    }
+          
+          
+          
+          
+     
