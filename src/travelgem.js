d4673363be9ab8471cc688c6015bbfd4792f8962
Adi Skop <adiskop@gmail.com>
2021-07-27 23:27:49 +0200

    trial of fixing bugs with form

--- src/travelgem.js
+++ src/travelgem.js
@@ -19,11 +19,12 @@ class Travelgem {
         <div data-id="${this.id}" class="col-md-4">
           <div class="card mb-4 shadow-sm">
           <img src=${this.image_url} class="card-img-top" alt="...">
-            
+          <div class="card text-center" >
 
             <div class="card-body" data-id="${this.id}">
             <h5 class="card-title">${this.title}</h5>
               <p class="card-text">${this.description}</p>
+              
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -36,16 +37,6 @@ class Travelgem {
         </div>
     
         `
-
-
-
-      //  return `<div data-id=${this.id}>
-       //   <img src=${this.image_url} height="200" width="250">
-        //  <h3>${this.title}</h3>
-          // <p>${this.destination.name}</p>
-        //  <button data-id=${this.id}>edit</button>
-       // </div>
-       // <br><br>`;
 
       }
 
